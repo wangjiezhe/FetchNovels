12b4286ce6732dff01bcfbdee66b7e92b3192379
wangjiezhe <wangjiezhe@gmail.com>
2014-11-09 04:11:54 +0800

    Download Novels from 123yq.com

---
+++ 123yq.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+#
+# 123yq.py
+# Copyright (c) 2014 Wang Jiezhe <wangjiezhe@gmail.com>
+# Released under GPLv3 or later.
+
+import os
+import re
+import urllib.request
+import json
+from bs4 import BeautifulSoup
+
+# NOVELS = ["鹂语记 - 七和香", "重生明珠 - 七和香", "富贵锦绣 - 飞翼",
+#           "丫鬟嫣然 - 秋李子", "雪满庭 - 颜竹佳"]
+URLS = ["http://www.123yq.com/read/20/20943/",
+        "http://www.123yq.com/read/5/5720/",
+        "http://www.123yq.com/read/22/22404/",
+        "http://www.123yq.com/read/22/22169/",
+        "http://www.123yq.com/read/25/25435/"]
+PROXY = {'http': '127.0.0.1:8087'}
+HEADERS = {
+    'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) \
+AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.101 Safari/537.36"
+}
+DEFAULT_BOOKMARK_FILE = os.path.expanduser(
+    '~/.config/chromium/Default/Bookmarks')
+# 由于 chardet 无法正确区分 gb2312 和 gbk, 故直接使用 gb18030.
+ENCODING = 'GB18030'
+
+
+def get_novels_from_chromium(bookmark_file=DEFAULT_BOOKMARK_FILE):
+    with open(bookmark_file, 'r') as fp:
+        bookmarks = json.load(fp)
+    for item in bookmarks['roots']['bookmark_bar']['children']:
+        if item['name'] == 'novel':
+            novels = item['children']
+            break
+    for item in novels:
+        if item['name'] == 'Finished':
+            novel_finished = item['children']
+            break
+    finished_list = []
+    finished_url_list = []
+    for item in novel_finished:
+        if item['url'].find('123yq') != -1:
+            match = re.match(r'(.+?),(.+?)\((.+?)\).+', item['name'])
+            finished_list.append(match.expand(r'\1 - \3'))
+            finished_url_list.append(item['url'])
+    return finished_list, finished_url_list
+
+
+def set_proxy(proxy=None):
+    if proxy is not None:
+        proxyhandler = urllib.request.ProxyHandler(proxy)
+    else:
+        proxyhandler = urllib.request.ProxyHandler()
+    opener = urllib.request.build_opener(proxyhandler)
+    urllib.request.install_opener(opener)
+
+
+class FetchNovel(object):
+    def __init__(self, url, headers=None, encoding='utf-8'):
+        self.url = url
+        if headers is None:
+            self.headers = {}
+        else:
+            self.headers = headers
+        self.encoding = encoding
+        self.index = self.get_index()
+        self.name = self.get_name()
+        self.chapter_url_list = self.get_chapter_urls()
+        self.download_dir = os.path.join(os.getcwd(), self.name)
+
+    def get_index(self):
+        req = urllib.request.Request(self.url, headers=self.headers)
+        with urllib.request.urlopen(req) as response:
+            index = response.read()
+        index = BeautifulSoup(index, from_encoding=self.encoding)
+        return index
+
+    def get_name(self):
+        novel = self.index.find_all('head')[0].text.strip()
+        match = re.match(r'(.+?),(.+?)\((.+?)\).+', novel)
+        return match.expand(r'\1 - \3')
+
+    def get_chapter_urls(self):
+        chapter_url_pattern = self.url + r'\d+?\.shtml'
+        chapter_urls = self.index.find_all(href=re.compile(chapter_url_pattern))
+        chapter_url_list = list(set(chapter_urls))
+        chapter_url_list.sort(key=chapter_urls.index)
+        return chapter_url_list
+
+    def download(self):
+        if not os.path.isdir(self.download_dir):
+            os.makedirs(self.download_dir)
+        for line in self.chapter_url_list:
+            filename = line.text + '.txt'
+            filepath = os.path.join(self.download_dir, filename)
+            if os.path.exists(filepath):
+                continue
+            req = urllib.request.Request(line['href'], headers=self.headers)
+            with urllib.request.urlopen(req) as response:
+                chapter = response.read()
+            chapter = BeautifulSoup(chapter, from_encoding=self.encoding)
+            content = chapter.find_all(id='TXT')[0]
+            text = str(content)
+            text = text.replace('\r', '')
+            text = text.replace('<br/>', '\n')
+            text = re.sub(r'<.+>', '', text)
+            with open(filepath, 'w') as fp:
+                fp.write(text)
+
+
+def main():
+    set_proxy(PROXY)
+    # for url in get_novels_from_chromium()[1]
+    for url in URLS:
+        novel = FetchNovel(url, HEADERS, ENCODING)
+        print("Fetching novel: %s" % novel.name)
+        novel.download()
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~END~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    input("Press <Enter> to quit!")
+
+
+if __name__ == '__main__':
+    main()
