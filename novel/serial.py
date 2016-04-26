4ac626d9ef4ff48c0f9b73427d2f72e4c16b67c5
wangjiezhe <wangjiezhe@gmail.com>
2016-04-26 18:32:07 +0800

    Update headers

--- novel/serial.py
+++ novel/serial.py
@@ -14,7 +14,7 @@ from .error import PropertyNotSetError, MethodNotSetError
 GOAGENT = {'http': '127.0.0.1:8087'}
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) \
-AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.101 Safari/537.36'}
+AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36'}
 
 
 class Page(object):
