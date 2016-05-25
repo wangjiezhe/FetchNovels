fcf289e594bd4b6769265d0fdac73a8af0ed7c70
wangjiezhe <wangjiezhe@gmail.com>
2016-05-25 11:08:55 +0800

    Add class for s-dragon

---
+++ sdragon.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from novel import single, utils, const
+
+BASE_URL = 'http://ebook.s-dragon.org/forum/archiver/?tid-{}.html'
+
+
+class Sdragon(single.Novel):
+
+    def __init__(self, tid, proxies=None):
+        super().__init__(utils.base_to_url(BASE_URL, tid),
+                         '.archiver_postbody',
+                         title_sel='h2',
+                         title_type=single.TitleType.selector,
+                         headers=const.HEADERS, proxies=proxies)
+
+
+def main():
+    utils.in_main(Sdragon, const.GOAGENT)
+
+
+if __name__ == '__main__':
+    main()
