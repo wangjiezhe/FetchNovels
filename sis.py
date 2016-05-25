ebbeefefb9020cba6689c93dc170eea8d4afcd5f
wangjiezhe <wangjiezhe@gmail.com>
2016-05-25 11:09:27 +0800

    Add class for sis001

---
+++ sis.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from novel import single, utils, const
+
+BASE_URL = 'http://www.sis001.com/forum/archiver/tid-{}.html'
+
+
+class Sis(single.Novel):
+
+    def __init__(self, tid, proxies=None):
+        super().__init__(utils.base_to_url(BASE_URL, tid),
+                         '.archiver_postbody',
+                         title_sel='h2',
+                         title_type=single.TitleType.selector,
+                         headers=const.HEADERS, proxies=proxies)
+
+
+def main():
+    utils.in_main(Sis, const.GOAGENT)
+
+
+if __name__ == '__main__':
+    main()
