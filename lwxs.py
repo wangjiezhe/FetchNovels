c312b61a8dcd293204b8e9fa42c65eb1c9db0cc0
wangjiezhe <wangjiezhe@gmail.com>
2016-06-01 21:08:04 +0800

    Add class for lwxs

---
+++ lwxs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+
+from pyquery import PyQuery as Pq
+
+from novel import serial, utils, const
+
+BASE_URL = 'http://www.lwxs.com/shu/{}/{}/'
+INTRO_URL = 'http://www.lwxs.com/info-{}.html'
+ENCODING = 'GB18030'
+
+
+class Lwxs(serial.Novel):
+
+    def __init__(self, tid, proxies=None):
+        super().__init__(utils.base_to_url(BASE_URL, tid), '.zhangjieTXT',
+                         utils.base_to_url(INTRO_URL, tid), '.intro',
+                         const.HEADERS, proxies, ENCODING,
+                         chap_sel='dd',
+                         chap_type=serial.ChapterType.last)
+
+    def get_title_and_author(self):
+        st = self.doc('meta').filter(
+            lambda i, e: Pq(e).attr('name') == 'keywords'
+        ).attr('content')
+        return re.match(r'(.*)最新章节\((.*?)\),.*', st).groups()
+
+
+def main():
+    utils.in_main(Lwxs, const.GOAGENT)
+
+
+if __name__ == '__main__':
+    main()
