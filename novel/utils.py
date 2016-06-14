0306ba6117a20ee8b28fc949815e57c2408dcbb7
wangjiezhe <wangjiezhe@gmail.com>
2016-06-14 11:13:06 +0800

    Refactor SqlHelper

--- novel/utils.py
+++ novel/utils.py
@@ -129,7 +129,7 @@ def count(iterable):
       >>> cardinality.count(gen())
       2
 
-    Get from https://github.com/wbolster/cardinality/blob/master/cardinality.py
+    Get from https://github.com/wbolster/cardinality
     """
     if hasattr(iterable, '__len__'):
         return len(iterable)
@@ -225,6 +225,11 @@ def is_sql_select(sql):
 
 
 class SqlHelper(Thread):
+    """
+    A thread-safe sqlite worker
+
+    Inspired by https://github.com/palantir/sqlite3worker
+    """
 
     def __init__(self, db):
         super().__init__()
@@ -232,28 +237,32 @@ class SqlHelper(Thread):
         self.queue = Queue()
         self.results = {}
         self.exit = False
-        self.exit_token = str(uuid4())
+        self._exit_token = str(uuid4())
         self.conn = sqlite3.connect(self.db, check_same_thread=False)
         self.cursor = self.conn.cursor()
         self.start()
+        self.running = True
 
     def run(self):
         for token, sql, args, many in iter(self.queue.get, None):
-            if token != self.exit_token:
-                self.query(token, sql, args, many)
+            if token != self._exit_token:
+                self._query(token, sql, args, many)
                 if self.queue.empty():
                     self.conn.commit()
 
             if self.exit and self.queue.empty():
                 self.conn.commit()
                 self.conn.close()
+                self.running = False
                 return
 
     def close(self):
         self.exit = True
-        self.queue.put((self.exit_token, '', (), 1))
+        self.queue.put((self._exit_token, '', (), 1))
+        while self.running:
+            sleep(0.01)
 
-    def query(self, token, sql, args, many):
+    def _query(self, token, sql, args, many):
         if is_sql_select(sql):
             try:
                 if many:
@@ -273,8 +282,7 @@ class SqlHelper(Thread):
             except sqlite3.Error as e:
                 print('Query error: {}: {}: {}'.format(sql, args, e))
 
-
-    def query_result(self, token):
+    def _query_result(self, token):
         delay = 0.001
         while True:
             if token in self.results:
@@ -284,12 +292,15 @@ class SqlHelper(Thread):
                 delay += delay
 
     def execute(self, sql=None, args=None, many=False):
+        if self.exit:
+            print('Exit called, not running: {}: {}'.format(sql, args))
+            return 'Exit called'
         sql = sql or ''
         args = args or ()
         token = str(uuid4())
         if is_sql_select(sql):
             self.queue.put((token, sql, args, many))
-            return self.query_result(token)
+            return self._query_result(token)
         else:
             self.queue.put((token, sql, args, many))
 
