718aa9d25d58e9465c7221f100d0c219c6de0d3c
wangjiezhe <wangjiezhe@gmail.com>
2016-06-23 13:12:51 +0800

    Sync novel list with database

--- novel/db.py
+++ novel/db.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import SingletonThreadPool
 
 from . import sources
-from .config import CACHE_DB
-from .models import Base, Serial
+from .config import CACHE_DB, load_novel_list, save_novel_list
+from .models import Base, Serial, Website
 
 
 def create_session(db=CACHE_DB):
@@ -22,11 +22,28 @@ def create_session(db=CACHE_DB):
     return session
 
 
-def update_all(db=CACHE_DB):
-    session = create_session(db)
+def update_all():
+    session = create_session()
     novel_list = session.query(Serial).all()
     for novel in novel_list:
         novel_class = getattr(sources, novel.source.capitalize())
         nov = novel_class(novel.id)
         nov.run()
     session.close()
+
+
+def sync_db_to_list():
+    session = create_session()
+    nl = {}
+    for w in session.query(Website).all():
+        nl[w.name] = [s.id for s in w.novels]
+    save_novel_list(nl)
+
+
+def sync_list_to_db():
+    nl = load_novel_list()
+    for s, tids in nl.items():
+        novel_class = getattr(sources, s.capitalize())
+        for tid in tids:
+            nov = novel_class(tid)
+            nov.run()
