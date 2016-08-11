398e6b4f3fc9473ae61833f3de6ea24b28a5e01e
wangjiezhe <wangjiezhe@gmail.com>
2016-08-11 19:10:14 +0800

    Auto detect packages

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@
 
 import sys
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from novel import __version__
+
+long_description = 'This project helps you to download novels from Internet, and easily write into files.'
 
 if __name__ == '__main__':
     assert sys.version_info.major > 2, 'Only works with python3'
@@ -13,13 +15,14 @@ if __name__ == '__main__':
         name='FetchNovels',
         version=__version__,
         description='Fetch novels from Internet',
+        long_description=long_description,
         license='GPLv3',
         url='https://github.com/wangjiezhe/FetchNovels',
 
         author='wangjiezhe',
         author_email='wangjiezhe@gmail.com',
 
-        packages=['novel', 'novel.sources'],
+        packages=find_packages(exclude=['docs', 'tools', 'tests*']),
         entry_points={
             'console_scripts': ['fetchnovels=novel.main:main'],
         },
