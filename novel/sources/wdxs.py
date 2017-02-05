35ec4121f894daaabce9970e302711b1af6a501b
wangjiezhe <wangjiezhe@gmail.com>
2017-02-05 22:26:47 +0800

    Fix tool for wdxs

--- novel/sources/wdxs.py
+++ novel/sources/wdxs.py
@@ -14,13 +14,26 @@ INTRO_URL = 'http://www.wodexiaoshuo123.com/{}/'
 class WdxsTool(utils.Tool):
 
     def __init__(self):
-        super().__init__(remove_div=False)
+        super().__init__(remove_div=False, remove_font=False)
+
+        self.remove_extras.extend(
+            (re.compile(pat) for pat in
+             (r'本文是龙马\nVIP文 特意购买希望大家喜欢,看龙马vip小说来91耽美网',
+              r'本文是龙马\nVIP文 特意购买希望大家喜欢',
+              '看特色销售就来我的小说网-',
+              ))
+        )
+
         self.remove_extras.extend(
             (re.compile(pat, re.I) for pat in
-             (r'www\.(wodexiaoshuo|01bz)\.(com|net|wang)',
-              r'(www|m)\.91danmei\.com',
-              r'wodexiaoshuo\.com',
-              r'www\.'))
+             (r'看.*?小.*?说.*?就.*?来.*?w.*?o.*?d.*?e.*?x.*?i.*?a.*?o.*?s.*?h.*?u.*?o.*?c.*?o.*?m',
+              r'w.*?w.*?w.*?w.*?o.*?d.*?e.*?x.*?i.*?a.*?o.*?s.*?h.*?u.*?o.*?(c.*?o.*?m|n.*?e.*?t)',
+              r'w.*?w.*?w.*?0.*?1.*?b.*?z.*?(c.*?o.*?m|n.*?e.*?t|w.*?a.*?n.*?g)',
+              r'(w.*?w.*?w|m).*?9.*?1.*?d.*?a.*?n.*?m.*?e.*?i.*?(c.*?o.*?m|n.*?e.*?t)',
+              r'w.*?o.*?d.*?e.*?x.*?i.*?a.*?o.*?s.*?h.*?u.*?o.*?c.*?o.*?m',
+              r'9.*?1.*?d.*?a.*?n.*?m.*?e.*?i.*?(c.*?o.*?m|n.*?e.*?t)',
+              r'w.*?w.*?w.*?\.',
+              ))
         )
         self.remove_extras.extend(
             (re.compile(pat) for pat in
