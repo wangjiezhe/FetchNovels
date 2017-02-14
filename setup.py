69fb41a24446240b269a1a8a57670d7043fc2fd2
wangjiezhe <wangjiezhe@gmail.com>
2017-02-14 11:29:51 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,5 +33,6 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
         ]
     )
