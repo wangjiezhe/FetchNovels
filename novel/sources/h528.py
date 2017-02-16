58715387c6120ebd5d25c45bd813fbef687cb010
wangjiezhe <wangjiezhe@gmail.com>
2017-02-16 12:24:38 +0800

    Add source for h528

---
+++ novel/sources/h528.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+
+from novel import single, utils
+
+BASE_URL = 'http://www.h528.com/post/{}.html'
+
+
+class H528Tool(utils.Tool):
+
+    def __init__(self):
+        super().__init__()
+        self.remove_extras.append(
+            re.compile(r'<!--.*?-->', re.S)
+        )
+
+
+class H528(single.SingleNovel):
+
+    def __init__(self, tid):
+        super().__init__(utils.base_to_url(BASE_URL, tid),
+                         '.post .entry',
+                         title_type=single.TitleType.selector,
+                         title_sel='.post h2',
+                         tid=tid)
+        self.tool = H528Tool
